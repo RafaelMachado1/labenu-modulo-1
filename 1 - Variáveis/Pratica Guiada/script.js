879edd1c42659253926ebbb86c4e354f2772e525
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2024-10-08 12:15:43 -0300

    pratica-guiada

---
+++ 1 - Variáveis/Pratica Guiada/script.js
@@ -0,0 +1,15 @@
+// let nome = prompt('Digite seu nome')
+// let idade = Number(prompt('Digita sua idade'))
+// let presenca = confirm ('presença')
+// console.log(typeof nome,idade,presenca)
+
+// console.log(`Estudante ${nome} tem ${idade} anos. Presença:${presenca}`)
+// console.log(typeof nome, typeof idade, typeof presenca)
+
+const celular =prompt ('Qual modelo do seu celular?')
+let tempo = Number(prompt('Há quanto tempo você tem ele?'))
+let trocar = prompt('Você pretende trocá-lo?')
+console.log(typeof celular,tempo,trocar)
+
+console.log(`Qual modelo do seu celular ${celular} e a quanto tempo você tem ele? ${tempo} anos.
+E você prentende trocá-lo? ${trocar}`)
