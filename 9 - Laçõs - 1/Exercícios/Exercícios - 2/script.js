25ff211081419bf34dec3952d6665402344adc66
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2024-10-17 14:55:04 -0300

    exercicios

---
+++ 9 - Laçõs - 1/Exercícios/Exercícios - 2/script.js
@@ -0,0 +1,6 @@
+let numUsuario = Number(prompt("Insira um número"))
+let tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(let i = 0 ; i < tabuada.length; i++){
+    console.log(numUsuario * tabuada[i])
+}
